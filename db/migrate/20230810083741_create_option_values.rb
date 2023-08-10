9de37533ae9a90f55ee538208aa3438858ae8efa
Vaneath <vaneathzz@gmail.com>
2023-08-10 17:27:00 +0700

    close #42 generate option value model and crud

---
+++ db/migrate/20230810083741_create_option_values.rb
@@ -0,0 +1,10 @@
+class CreateOptionValues < ActiveRecord::Migration[7.0]
+  def change
+    create_table :option_values do |t|
+      t.references :option_type, null: false, foreign_key: true
+      t.string :value
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_080000) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_10_083741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_080000) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "option_values", force: :cascade do |t|
+    t.bigint "option_type_id", null: false
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["option_type_id"], name: "index_option_values_on_option_type_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -61,5 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_080000) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "option_values", "option_types"
   add_foreign_key "variants", "products"
 end
