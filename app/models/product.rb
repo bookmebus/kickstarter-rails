838f65ce1e950d4c5dec77caf88cc134ade6ed77
Vaneath <vaneathzz@gmail.com>
2023-08-10 09:12:09 +0700

    close #32 crud on variant

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-    validates: name, presence: true
-    validates: description
+    validates :name, presence: true
+    validate :description
 
     has_many :product_variants
     has_many :product_details, through: :product_variants
